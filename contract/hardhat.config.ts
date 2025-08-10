e8a8f77d933ab11fcae0c69343a7cf276ee27d7d
Haruki Kondo <44923695+mashharuki@users.noreply.github.com>
2025-08-10 05:57:34 +0000

    雛形を追加

---
+++ contract/hardhat.config.ts
@@ -0,0 +1,22 @@
+import type { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox-viem';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+  PRIVATE_KEY,
+  KAIGAN_RPC_URL,
+} = process.env;
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.28',
+  networks: {
+    kaigan: {
+      url: KAIGAN_RPC_URL!,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
